0cc4fc56f7a7b82eac89cc4bca805e99358bbeee
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-13 21:10:56 +0300

    updated: print() and println() methods

--- app/src/main/java/com/ismailmesutmujde/kotlinvariablesanddatatypes/CreatingVariable.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinvariablesanddatatypes/CreatingVariable.kt
@@ -16,14 +16,22 @@ fun main() {
     // Example 2
 
     var product_id : Int = 3416
-    var product_name : String = "Watch"
+    var product_name : String = "Kol saati"
     var product_quantity : Int = 100
     var product_price : Double = 109.99
     var product_supplier : String = "rolex"
 
-    println(product_id)
-    println(product_name)
-    println(product_quantity)
-    println(product_price)
-    println(product_supplier)
+    println("Product id       : $product_id")
+    println("Product name     : $product_name")
+    println("Product quantity : $product_quantity")
+    println("Product price    : $product_price")
+    println("Product supplier : $product_supplier")
+
+    // Example 3
+    println("$product_supplier marka $product_name $product_price fiyatla satılmaktadır. Stokta $product_quantity adet kalmıştır.")
+
+    // Example 4
+    var a = 10
+    var b = 20
+    println("$a ve $b nin toplamı : ${a+b}")
 }
