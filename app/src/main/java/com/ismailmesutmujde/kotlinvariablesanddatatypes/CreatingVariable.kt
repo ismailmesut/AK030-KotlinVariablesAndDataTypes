67b5a54ac8e8b4f886a8de4d0bd531f344e297f5
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-13 20:40:18 +0300

    updated: create variables

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinvariablesanddatatypes/CreatingVariable.kt
@@ -0,0 +1,29 @@
+package com.ismailmesutmujde.kotlinvariablesanddatatypes
+
+fun main() {
+
+    // Example 1
+    var studentName = "ismail"
+    var studentAge = 23
+    var studentHeight = 1.98
+    var studenNameChar = 'i'
+
+    println(studentName)
+    println(studentAge)
+    println(studentHeight)
+    println(studenNameChar)
+
+    // Example 2
+
+    var product_id : Int = 3416
+    var product_name : String = "Watch"
+    var product_quantity : Int = 100
+    var product_price : Double = 109.99
+    var product_supplier : String = "rolex"
+
+    println(product_id)
+    println(product_name)
+    println(product_quantity)
+    println(product_price)
+    println(product_supplier)
+}
