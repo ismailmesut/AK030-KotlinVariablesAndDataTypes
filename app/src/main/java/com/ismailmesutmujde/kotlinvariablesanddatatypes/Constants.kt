f8ab43825304e1f5e38c5efbc6965239acb09358
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-14 12:31:05 +0300

    updated: constants

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinvariablesanddatatypes/Constants.kt
@@ -0,0 +1,18 @@
+package com.ismailmesutmujde.kotlinvariablesanddatatypes
+
+fun main() {
+    var number = 10
+    number = 20
+    number = 40
+    println(number)
+
+    // Constants
+    val number2 = 100
+    //number2 = 50 // hata verir.
+
+    val pi = 3.14
+
+    val chlorineRate : Double = 4.5
+
+    val name = "ismail"
+}
