ddc1b4702d4fbad1bce5a4c9bd22cb06861aa182
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-14 15:17:12 +0300

    updated: console input

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinvariablesanddatatypes/ConsoleInput.kt
@@ -0,0 +1,10 @@
+package com.ismailmesutmujde.kotlinvariablesanddatatypes
+
+import java.util.Scanner
+
+fun main() {
+    println("Enter your name : ")
+    val input = Scanner(System.`in`)
+    val name = input.next()
+    println("Your name : $name")
+}
