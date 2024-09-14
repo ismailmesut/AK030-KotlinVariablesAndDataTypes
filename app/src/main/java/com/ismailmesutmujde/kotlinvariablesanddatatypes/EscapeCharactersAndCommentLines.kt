5bdc34440ec6fc52ab59616b1007bf6202a3ea02
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-14 12:42:04 +0300

    updated: escape characters and comment lines

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinvariablesanddatatypes/EscapeCharactersAndCommentLines.kt
@@ -0,0 +1,19 @@
+package com.ismailmesutmujde.kotlinvariablesanddatatypes
+
+fun main() {
+
+    // Example 1
+    var text = "İsmail \"Nasılsın\" dedi"   // İsmail "Nasılsın" dedi
+    println(text)
+
+    // Example 2
+    var text2 = "Merhaba bu \"android\"\n\teğitiminde \\kotlin\\ dilini öğreneceğiz."
+    println(text2)
+
+    // Single Line Comment
+
+    /*
+        Multi Line Comment
+
+     */
+}
