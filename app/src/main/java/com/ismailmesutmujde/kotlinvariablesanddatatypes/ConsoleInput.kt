137241cc303411cdffb054f74e6a2f9d913b6965
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-14 15:23:30 +0300

    updated: debugging

--- app/src/main/java/com/ismailmesutmujde/kotlinvariablesanddatatypes/ConsoleInput.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinvariablesanddatatypes/ConsoleInput.kt
@@ -3,8 +3,18 @@ package com.ismailmesutmujde.kotlinvariablesanddatatypes
 import java.util.Scanner
 
 fun main() {
-    println("Enter your name : ")
+    //println("Enter your name : ")
+    println("Enter number")
     val input = Scanner(System.`in`)
-    val name = input.next()
-    println("Your name : $name")
+    //val name = input.next()
+    val number = input.nextInt()
+    //println("Your name : $number")
+    println("Result : $number")
+
+    // hata verir..
+    /*
+    var x = 5
+    var y = x / 0
+
+     */
 }
