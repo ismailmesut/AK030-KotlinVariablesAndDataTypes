cae46acd41b22824413dee8a71e883f1adef3663
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-14 11:57:08 +0300

    updated: creating variable types

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinvariablesanddatatypes/CreatingVariableTypes.kt
@@ -0,0 +1,24 @@
+package com.ismailmesutmujde.kotlinvariablesanddatatypes
+
+fun main() {
+
+    // Example 1
+    var number = 10
+
+    // Example 2
+    var price = 12.99
+    println(price)
+    price = 10.99
+    println(price)
+
+    // Example 3
+    var s1 = 80
+    var s2 = 70
+    var sum = s1 + s2
+    println(sum)
+
+    // Example 4 : Type Safety
+
+    var result = 100
+    //result = 1.98  // hata verir.
+}
